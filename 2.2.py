aef13220bac7aed619b55b3ddd6953a6027a6e09
jenishdanej <jenishhdanej20@gmail.com>
2025-05-31 17:29:17 +0530

    Add files via upload

---
+++ 2.2.py
@@ -0,0 +1,13 @@
+x = int(input("enter value of x"))
+y = int(input("enter value of y"))
+op = input ("enter oprrator (+,-,*,\)")
+if op == '+' :
+    print (x + y)
+elif op == '-' :
+    print (x - y)
+elif op == '*' :
+    print (x * y)
+elif op == '/' :
+    print == (x / y )
+else :
+    print("invalid op")
