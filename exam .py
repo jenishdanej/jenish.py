9ac09d8b41d83e560f8c7ef7d845f34c88d27a91
jenishdanej <jenishhdanej20@gmail.com>
2025-05-31 17:11:33 +0530

    exam.py1

---
+++ exam .py
@@ -0,0 +1,16 @@
+print("welcome to the interactive personal data collector")
+a = input("enter your name")
+x = input("enter your age")
+y = input("enter your heigth")
+z = input("enter your favourtive number")
+
+
+
+name = type(str)
+age = type(int)
+height = type(float)
+favourite = type(int)
+print (str)
+print (int)
+print(float)
+print(int)
