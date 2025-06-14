362f19bb4c451764967c220df75a353e79c7a532
jenishdanej <jenishhdanej20@gmail.com>
2025-06-14 18:47:14 +0530

    Add files via upload

---
+++ exam 2.py
@@ -0,0 +1,14 @@
+input ("enter total bill amount"):
+a = float(input ("enter number of people")):
+
+b = int(input ("enter enter tip percentage")):
+while True:
+    if (b < 0)
+    
+a = int(iput ("enter number of people") (*))
+
+
+
+     
+         
+
